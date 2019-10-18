012085471671c740680a5004da0061abeff52f8b
Anderson <anderson.soares99@hotmail.com>
2019-10-17 21:54:01 -0300

    gobarber-3

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello And'});
-})
-
+  return res.json({ message: 'Hello And' });
+});
 export default routes;
