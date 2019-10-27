06a82bdbbdd85b32df6f21a9e7b990ee4ad513ca
Anderson <anderson.soares99@hotmail.com>
2019-10-27 20:27:34 -0300

    gobarber-4-add Atualization

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello And' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Anderson Soares',
+    email: 'anderson.soares99@hotmail.com',
+    password_hash: '123456',
+  });
+
+  return res.json({ user });
 });
 export default routes;
