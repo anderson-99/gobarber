aeccf6024a53271500e54ab785f3db203987c920
Anderson <anderson.soares99@hotmail.com>
2019-11-01 09:43:22 -0300

    gobarber-6-add Atualization

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -17,6 +17,31 @@ class UserController {
       provider,
     });
   }
+
+  // para atualização da senha do usuário
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ email });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
